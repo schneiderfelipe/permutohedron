5ed3d70c739e6f8674833c39cd981344f881996b
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2024-05-14 11:34:54 -0300

    revert(src/control.rs): remove unused continuing method in control::ControlFlow

--- src/control.rs
+++ src/control.rs
@@ -17,30 +17,21 @@ macro_rules! try_control {
 /// The empty return value `()` is equivalent to continue.
 #[allow(clippy::module_name_repetitions)]
 pub trait ControlFlow {
-    fn continuing() -> Self;
     #[inline]
     fn should_break(&self) -> bool {
         false
     }
 }
 
-impl ControlFlow for () {
-    fn continuing() {}
-}
+impl ControlFlow for () {}
 
 impl<B> ControlFlow for std::ops::ControlFlow<B, ()> {
-    fn continuing() -> Self {
-        Self::Continue(())
-    }
     fn should_break(&self) -> bool {
         matches!(self, Self::Break(_))
     }
 }
 
 impl<E> ControlFlow for Result<(), E> {
-    fn continuing() -> Self {
-        Ok(())
-    }
     fn should_break(&self) -> bool {
         self.is_err()
     }
